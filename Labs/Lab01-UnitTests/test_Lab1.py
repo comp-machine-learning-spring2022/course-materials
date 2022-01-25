06569ab6f5a8a918ca241c8116af5d6c71f8c974
Katherine M. Kinnaird <kkinnaird@smith.edu>
2022-01-25 17:12:52 -0500

    adding Lab 1

---
+++ Labs/Lab01-UnitTests/test_Lab1.py
@@ -0,0 +1,18 @@
+import pytest
+import labone 
+import os
+
+path, _ = os.path.split(os.path.abspath(__file__))
+
+def test_importdata():
+	expected = (517,13)
+	fname = os.path.join(path,"forestfires.csv")
+	assert labone.importdata(fname).shape == expected
+
+def test_addingthings():
+	expected = 7
+	assert labone.addingthings(3,4) == expected
+
+def test_addfive():
+	expected = 12
+	assert labone.addfive(7) == expected
